7dfe4146605dcff053f7d78677884a0b3f94adad
dev0_gb97 <dev0_gb97@outlook.com>
2023-08-23 11:00:09 -0500

    restructuracion de videos pedidos

--- interface/PedidoVideos.js
+++ interface/PedidoVideos.js
@@ -1,56 +1,63 @@
-const GB97Path = "https://gb97.ec/videos_nuevos"
+const GB97Path = "https://bucket-images-gb97.s3.amazonaws.com/upload/webpage/videos-guia"
 export const Pedido = [
     {
-        key: "Video1",
-        title: "Creación de Clientes",
-        path: `${GB97Path}/pedidoscreacliente.mp4`,
-        preview: "Creacion_Clientes.png",
+        key: "Video Información General",
+        title: "Generalidades",
+        path: `${GB97Path}/pedidosinfgeneral.mp4`,
+        previe: "Informacion_General.png",
         alt: "video"
     },
     {
-        key: "Video2",
+        key: "Video Creación de Productos",
         title: "Creación de Productos",
         path: `${GB97Path}/pedidoscreaitem.mp4`,
         preview: "Creacion_Items.png",
         alt: "video"
     },
     {
-        key: "Video3",
-        title: "Creación de Pedidos",
-        path: `${GB97Path}/pedidoscreapedido.mp4`,
-        preview: "Creacion_Pedido.png",
-        alt: "video"
-    },
-    {
-        key: "Video4",
-        title: "Gestión de Productos ( Editar, Eliminar y Duplicar Items )",
+        key: "Video Gestión de Productos",
+        title: "Edición de productos, eliminar, duplicar items",
         path: `${GB97Path}/pedidosmodificaitem.mp4`,
         preview: "Modificacion_Item.png",
         alt: "video"
     },
     {
-        key: "Video5",
+        key: "Video Creación de Clientes",
+        title: "Creación de Clientes",
+        path: `${GB97Path}/pedidoscreacliente.mp4`,
+        preview: "Creacion_Clientes.png",
+        alt: "video"
+    },
+    {
+        key: "Video Lista de Precios",
         title: "Lista de Precios",
         path: `${GB97Path}/pedidoslistaprecios.mp4`,
         preview: "Lista_Precios.png",
         alt: "video"
     },
     {
-        key: "Video6",
+        key: "Video Emision de Proformas",
         title: "Emision de Proformas",
         path: `${GB97Path}/pedidosemisproforma.mp4`,
         preview: "Emision_Proformas.png",
         alt: "video"
     },
     {
-        key: "Video7",
+        key: "Video Creación de Pedidos",
+        title: "Creación de Pedidos",
+        path: `${GB97Path}/pedidoscreapedido.mp4`,
+        preview: "Creacion_Pedido.png",
+        alt: "video"
+    },
+    {
+        key: "Video Historial de Pagos",
         title: "Historial de Pagos",
         path: `${GB97Path}/pedidoshistopagos.mp4`,
         preview: "Historial_Pagos.png",
         alt: "video"
     },
     {
-        key: "Video8",
+        key: "Video Factura Electronica",
         title: "Factura Electronica",
         path: `${GB97Path}/pedidosfacelectronica.mp4`,
         preview: "Factura_Electronica.png",
