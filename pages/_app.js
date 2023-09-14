d82a8f679a229bc1ac81ff083c6a0804ac1752b9
dev0_gb97 <dev0_gb97@outlook.com>
2023-09-14 10:32:03 -0500

    google script

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,29 @@
 import "../styles/scss/style.scss";
 import Layout from "../layout/Layout";
-import Script from "next/script";
 import { AuthProvider } from "../context/AuthContext";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <Layout>
-        <Script
-          src={"https://www.googletagmanager.com/gtag/js?id=G-S6V94DDDNW"}
-        />
-        <Script id="google-analytics">
-          {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'G-S6V94DDDNW');
-            `}
-        </Script>
-        <Component {...pageProps} />
-      </Layout>
-    </AuthProvider>
+    <>
+      <div className="container">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-S6V94DDDNW" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-S6V94DDDNW');
+        `}
+      </Script>
+    </div>
+      <AuthProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AuthProvider>
+    </>
   );
 }
 
