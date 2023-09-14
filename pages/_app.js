676dccddc2f6c6d2b1976e6edeb8872cb2d94706
dev0_gb97 <dev0_gb97@outlook.com>
2023-09-14 09:41:44 -0500

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,25 @@
 import "../styles/scss/style.scss";
 import Layout from "../layout/Layout";
+import Script from "next/script";
 import { AuthProvider } from "../context/AuthContext";
+import GoogleAnalytics from "../components/basic/GoogleAnalytics";
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
       <Layout>
+        <Script
+          src={"https://www.googletagmanager.com/gtag/js?id=G-S6V94DDDNW"}
+        />
+        <Script id="google-analytics">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-S6V94DDDNW');
+            `}
+        </Script>
         <Component {...pageProps} />
       </Layout>
     </AuthProvider>
