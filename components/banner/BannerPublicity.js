79cd873ecc59fe835e2644985f7142434c7b4e02
dev0_gb97 <dev0_gb97@outlook.com>
2023-09-27 12:00:06 -0500

    banner gif

--- components/banner/BannerPublicity.js
+++ components/banner/BannerPublicity.js
@@ -6,10 +6,10 @@ const BannerPublicity = () => {
     <div className="banner-publicity-container">
       <Image 
         className="banner-image"
-        src="https://bucket-images-gb97.s3.amazonaws.com/upload/webpage/images/Ban.webp"
+        src="https://bucket-images-gb97.s3.amazonaws.com/upload/webpage/images/Ban.gif"
         loading='lazy'
         alt="banner"
-        width={"100%"}
+        width={"75%"}
         height={200}
         />
     </div>
