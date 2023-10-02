77cb91238711bbcb50579823fb4879c2f2cc5bd8
dev0_gb97 <dev0_gb97@outlook.com>
2023-10-02 10:38:59 -0500

    add carusel

--- components/banner/Banner2.js
+++ components/banner/Banner2.js
@@ -1,29 +1,46 @@
 import React from "react";
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row, Col} from "reactstrap";
 import Image from "next/image";
 
 import iso from "../../assets/images/landingpage/iso.png";
-import home from "../../assets/images/landingpage/home.png";
+import ImagesCarousel from "../basic/ImagesCarousel";
+import BannerPublicity from "../banner/BannerPublicity"
 
 
 const Banner2 = () => {
   return (
     <div className="static-slider-head banner2">
+      <BannerPublicity />
       <Container>
-        <Row className="">
-          <Col lg="6" md="6" sm='5' xs='12' className="align-self-center">
+        <Row className="d-flex">
+          <Col xl="6" lg="6" md="12" sm='12' xs='12' className="align-self-center">
+            {/* <div className="d-flex justify-content-center">
+              <Image 
+                src={"https://bucket-images-gb97.s3.amazonaws.com/upload/0691776360001.png"}
+                width={200}
+                height={200}
+                alt="Logo"
+              />
+            </div> */}
             <h1 className="title">
-              SOLUCIONES TECNOLÓGICAS
+              Misión
             </h1>
             <h4 className="subtitle font-light text-justify mr-5">
-              En GB97 Ecuador nuestra prioridad es que nuestros productos cumplan con estándares de calidad y que se adapten a sus necesidades. Contamos con una certificación ISO 9001.
+              Somos una empresa que ofrece soluciones tencológicas a compañías y/o emprendimientos de la industria textil. 
+              En donde sistematizando los procesos de producción y calidad, las empresas desarrollen productos de calidad, los cuáles sean competitivos en el mercado nacional e internacional.
             </h4>
-            <div className="image-iso d-flex justify-content-end mr-5 pt-3 justify-content-xs-center">
-              <Image src={iso} alt="Logo ISO" width={250} height={120}/>            
+            <h1 className="title">
+              Vision
+            </h1>
+            <h4 className="subtitle font-light text-justify mr-5">
+              Ser una empresa líder en el desarrollo de soluciones tecnológicas textiles, adaptándonos a los estándares de calidad nacionales e internacionales para el manejo y desarrollo de procesos sistematizados de empresas y/o emprendimientos de la industria textil.
+            </h4>
+            <div className="image-iso d-flex mr-5 mb-5 pt-3">
+              <Image src={iso} alt="Logo ISO" width={160} height={90}/>            
             </div>
           </Col>
-          <Col lg="6" md="6" sm='5' xs={{size: 'auto'}}>
-            <Image className="image-home" src={home} alt="Banner Inicio"/>
+          <Col xl="6" lg="6" md="12" sm='12' xs='12' className="align-self-center pb-5 mb-5">
+            <ImagesCarousel />
           </Col>
         </Row>
       </Container>
