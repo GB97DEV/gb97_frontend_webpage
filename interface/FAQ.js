d8fbce855f7b63a3cc427d6a61a59284617d4e90
dev0_gb97 <dev0_gb97@outlook.com>
2024-03-14 11:32:42 -0500

    Nuevas preguntas

--- interface/FAQ.js
+++ interface/FAQ.js
@@ -34,5 +34,61 @@ export const FAQ = [
   {
     question: "Preguntas acerca del RPN o información propia de procedimientos relacionados con entidades públicas",
     answer: "GB97 Ecuador es una empresa privada que se dedica a la creación de aplicaciones móviles, algunas de ellas han sido desarrolladas para apoyar al sector textil, es necesario aclarar que no tiene relación ni influencia alguna en los procedimientos y requisitos de alguna entidad pública, por lo que sugerimos informarse en los canales adecuados de las propias entidades."
+  },
+  {
+    question: "¿Por qué aparece el mensaje “El usuario o la contraseña son incorrectos” cuando intento acceder a la app de Pedidos?",
+    answer: "Es probable que estemos ingresando un espacio con el teclado, al inicio o al final de nuestro usuario o contraseña, esto sucede cuando tenemos activado el autocorrector en el teclado. Se soluciona revisando que no existan espacios después de escribir nuestras credenciales, activando el ícono de ojo en el campo de contraseña.",
+  },
+  {
+    question: "¿Por qué tengo que ingresar a cada momento mi usuario y contraseña?",
+    answer: "Esto sucede cuando damos click en Cerrar Sesión en lugar de dejarla abierta en segundo plano. La sesión se mantendrá abierta hasta el día siguiente que será necesario volver a ingresar las credenciales.",
+  },
+  {
+    question: "¿Por qué la aplicación en ocasiones se ralentiza o aparece mi pantalla en negro?",
+    answer: "Es necesario verificar que tengamos instalada la última versión de la aplicación directamente en el Play Store, una vez instalada la actualización, la aplicación funcionará en óptimas condiciones",
+  },
+  {
+    question: "¿Cómo puedo cambiar la contraseña de mi aplicación?",
+    answer: "En la pantalla de inicio existe la opción ¿Olvidó su contraseña?, seleccionando esta opción debemos digitar el nombre de usuario y seleccionar Reestablecer contraseña, a continuación, llegará un correo electrónico al mail de respaldo, con un código de 6 dígitos que debemos ingresar acompañado de la nueva contraseña que deseamos establecer y el proceso de cambio se habrá realizado con éxito.",
+  },
+  {
+    question: "¿Cómo puedo cambiar la información de mi negocio para que aparezca en los documentos pdf?",
+    answer: "Para este proceso es necesario ingresar a la Aplicación de pedidos, elegir el módulo Mi Organización y cambiar los datos que se requieran, razón social, teléfono, correo, dirección, una vez guardada esta información los documentos aparecerán con los datos actualizados.",
+  },
+  {
+    question: "¿Por qué no me aparece la opción de Proceso de Entrega en mis pedidos?",
+    answer: "Para que se active el proceso de entrega la orden debe encontrarse en estado Finalizado, caso contrario el ícono de entrega no aparecerá. Debe ingresar a Ver detalles de la orden, en la barra de estado, normalmente de color celeste con el mensaje Orden Ingresada, dar un click y elegir el estado Orden Finalizada, una vez que regreso al listado de órdenes se encontrará con color rojo y con el ícono de camión.",
+  },
+  {
+    question: "¿Por qué no puedo visualizar los pdf?",
+    answer: "Esto sucede cuando no tenemos instalada una aplicación para la apertura de pdf, se recomienda contar con la app Drive o la app Adobe Reader, estas dos aplicaciones se encuentran en el play store y abren pdf sin mostrar publicidad ni solicitar cobro alguno, por lo que se puede manejar nuestros documentos de forma segura y compartirlos con la opción de enviar archivo a través de Whatsapp o la app de preferencia.",
+  },
+  {
+    question: "¿Qué tipo de producto o ítem debería utilizar de acuerdo al giro de mi negocio?",
+    answer: "Si su negocio textil se enfoca en la fabricación bajo pedido, en la que, independientemente de la talla del producto el precio se mantiene recomendamos utilizar Ítems de Producción. En caso de que el giro de su negocio se relacione a venta de uniformes, venta de ropa de hogar diferenciadas por plazas, prendas que varíen su precio de acuerdo a talla recomendamos usar Ítems variantes.",
+  },
+  {
+    question: "En caso de que el cliente me solicite añadir una característica adicional a la prenda que está comprando, que aumenta el precio de la misma, ¿debo crear otro producto?",
+    answer: "Se recomienda en este caso, en lugar de crear un nuevo producto, utilizar la opción Complementos, aquí podrá tener un listado de los aditamentos y precio que se sumarán al costo final de la prenda una vez elegido el complemento. De esta forma evitaremos crear varios productos y cada vez que se necesite añadir una característica adicional a la prenda lo seleccionaremos del listado de complementos.",
+  },
+  {
+    question: "Si manejo precios diferenciados de un producto para cada cliente, ¿debo crear un nuevo ítem con el nombre del cliente?",
+    answer: "No es necesario realizarlo, lo recomendable es utilizar la opción Lista de Precios, en esta opción se puede elegir un cliente y cambiar el precio base del producto con el valor específico que se manejará con el cliente, cada vez que requiera enviar una proforma o un pedido el precio que aparecerá será el precio establecido en la lista. De esta forma, no será necesario crear varios productos con el nombre del cliente sino utilizar el módulo Lista de precios.",
+  },
+  {
+    question: "¿Qué puedo hacer si mi cliente me informa que no recibió el correo con el pedido o la proforma?",
+    answer: "En este caso podemos ingresar a la opción Ver detalles de la proforma o del pedido, dirigirnos a la parte inferior de la pantalla y seleccionar la opción Descargar, en ese momento se generará y descargará un documento pdf, el mismo que puede enviar directamente a su cliente a través de whatsapp.",
+  },
+  {
+    question: "¿Por qué no me aparece la foto de mi producto en la proforma que estoy emitiendo?",
+    answer: "La carga de fotografías es opcional dentro de la aplicación, por lo que no se incluyen dentro de las proformas para evitar que queden espacios en blanco. Una opción recomendable si deseamos enviar las fotografías del producto al cliente es utilizar la opción Descargar PDF ingresando directamente al listado de productos y sus detalles, de esta manera obtenemos un documento que constará con la descripción del producto y hasta 6 fotografías, denominado Ficha informativa del producto.",
+  },
+  {
+    question: "¿Puedo utilizar la aplicación de Pedidos Textil siendo Persona Natural, dentro del régimen RIMPE Negocios Populares, puesto que no necesito emitir facturas electrónicas sino notas de venta?",
+    answer: "Puede utilizar la aplicación de pedidos sin inconveniente, puesto que el módulo de facturación se habilita únicamente si el cliente lo solicita, caso contrario podrá utilizar los demás módulos sin problema, generar proformas, pedidos, catálogo de productos, historial de pagos y entregas sin inconvenientes.",
+  },
+  {
+    question: "¿Qué necesito para activar dentro de mi aplicación el módulo de Facturación electrónica?",
+    answer: "Necesita disponer de la firma electrónica, en formato p12, del representante legal en caso de ser una persona jurídica o del titular en caso de ser una persona natural. Necesita además contar con un logotipo y el RUC actualizado, estos dos documentos deberán enviarse previamente al mail servicioalcliente@gb97.ec indicando que se requiere activar la facturación electrónica en la aplicación de pedidos. Una vez solicitado este servicio se enviará al correo electrónico de contacto un link para continuar con el proceso, en este link se deberá subir la contraseña y el archivo de firma electrónica. El link se encuentra activo por 30 minutos una vez generado, pasado este tiempo debe volver a solicitar un link. Una vez realizado esperaremos la notificación para poder hacer uso de este servicio",
   }
 ]
